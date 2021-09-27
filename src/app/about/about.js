7323e69d0029708494b444ed0015e90695a55432
trminhphu79 <asagosv2@gmail.com>
2021-09-27 20:11:14 +0700

    first commit

---
+++ src/app/about/about.js
@@ -0,0 +1,7 @@
+let renderAboutPage = () => {
+    return `
+    <div class="container">
+         <h1>ABOUT PAGE</h1>
+    </div>
+    `
+}
---
+++ src/app/home/home.js
@@ -0,0 +1,7 @@
+let renderHomePage = () => {
+    return `
+    <div class="container">
+        <h1>Home PAGE</h1>
+    </div>
+    `
+}
